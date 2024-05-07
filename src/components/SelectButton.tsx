57d287cf5ee24176c7ffeb08f68dfc6fd544855f
ParkSohyunee <sohyunee016@gmail.com>
2024-05-07 23:49:35 +0900

    Design: SelectMenu 컴포넌트 UI 구현

--- src/components/SelectButton.tsx
+++ src/components/SelectButton.tsx
@@ -1,10 +1,26 @@
+import { fruits } from "mock-datas/data";
 import styles from "./SelectButton.module.css";
+
+function SelectMenu() {
+  return (
+    <ul className={styles.container}>
+      {["Fruits", ...fruits].map((fruit) => (
+        <li key={fruit} className={styles.item}>
+          {fruit}
+        </li>
+      ))}
+    </ul>
+  );
+}
 
 export default function SelectButton() {
   return (
-    <button className={styles.button}>
-      <span>Select a fruit</span>
-      <img className={styles.icon} src="/icon/arrow_up.svg" alt="메뉴 보기" />
-    </button>
+    <div>
+      <button className={styles.button}>
+        <span>Select a fruit</span>
+        <img className={styles.icon} src="/icon/arrow_up.svg" alt="메뉴 보기" />
+      </button>
+      <SelectMenu />
+    </div>
   );
 }
