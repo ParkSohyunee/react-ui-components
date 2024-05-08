028b2a8d4e41c29c2f8451160ec747b67a3d1aa2
ParkSohyunee <sohyunee016@gmail.com>
2024-05-08 21:12:26 +0900

    feat: Select 버튼 클릭시 menu dropdown 컴포넌트 구현

--- src/components/SelectButton.tsx
+++ src/components/SelectButton.tsx
@@ -1,9 +1,16 @@
+import { useEffect, useRef, useState } from "react";
+import classNames from "classnames";
+
 import { fruits } from "mock-datas/data";
 import styles from "./SelectButton.module.css";
 
-function SelectMenu() {
+interface SelectMenuProps {
+  isOpen: boolean;
+}
+
+function SelectMenu({ isOpen }: SelectMenuProps) {
   return (
-    <ul className={styles.container}>
+    <ul className={classNames(styles.container, isOpen ? styles.visible : "")}>
       {["Fruits", ...fruits].map((fruit) => (
         <li key={fruit} className={styles.item}>
           {fruit}
@@ -14,13 +21,37 @@ function SelectMenu() {
 }
 
 export default function SelectButton() {
+  const ref = useRef<HTMLDivElement>(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  // 셀렉트 메뉴 이외의 공간 클릭 감지
+  useEffect(() => {
+    const onClick = (e: MouseEvent) => {
+      if (ref.current !== null && !ref.current.contains(e.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+
+    if (isOpen) {
+      window.addEventListener("click", onClick);
+    }
+
+    return () => {
+      window.addEventListener("click", onClick);
+    };
+  }, [isOpen]);
+
   return (
-    <div>
-      <button className={styles.button}>
+    <div ref={ref} className={styles.wrapper}>
+      <button onClick={toggleModal} className={styles.button}>
         <span>Select a fruit</span>
         <img className={styles.icon} src="/icon/arrow_up.svg" alt="메뉴 보기" />
       </button>
-      <SelectMenu />
+      <SelectMenu isOpen={isOpen} />
     </div>
   );
 }
